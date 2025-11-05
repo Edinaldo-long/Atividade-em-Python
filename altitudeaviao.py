d44c3598d4ecff18c1d945ec6fd09e3fbfc8fc5e
Edinaldo Santos de Almeida <esatakeu@gmail.com>
2025-11-05 20:47:02 -0300

    Implement altitude check for airplane
    Add altitude control logic for the airplane.

--- altitudeaviao.py
+++ altitudeaviao.py
@@ -1,3 +1,12 @@
+'''
+Exercício — Controle de Altitude O comandante precisa verificar se o avião está voando na altitude correta para
+ a rota. Crie um programa em Python que: - Peça ao usuário para digitar a altitude atual do avião (em pés) e mostre
+ na tela uma mensagem de acordo com o valor digitado. Se a altitude estiver entre 30.000 e 40.000 pés,
+ mostre: "Altitude ideal! Mantenha o nível de voo." Se a altitude for menor que 30.000 pés, mostre:
+ "Suba mais um pouco!" Se a altitude for maior que 40.000 pés, mostre: "Desça imediatamente!
+'''
+
+
 altitude = int(input("Digite a altitude atual do avião (em pés): "))
 
 if 30000 <= altitude <= 40000:
