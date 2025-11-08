914089554ec5e3bdfcd711ba5091e4296b4a8f4e
Edinaldo Santos de Almeida <esatakeu@gmail.com>
2025-11-08 18:21:29 -0300

    Dicionário e lista - feirinha

---
+++ dicionariofeirinhado juninho.py
@@ -0,0 +1,20 @@
+# Lista para armazenar Feirinha do Juninho
+prateleira = []
+
+while True:
+    nome = input("Digite o nome do produto: ")
+    preco = float(input("Digite o preço do produto: R$ "))
+    setor = input("Digite a setor do hortifrut : ")
+
+    # Cada produto é um dicionário, adicionado à lista
+    produto = {"nome": nome, "preco": preco, "setor": setor}
+    prateleira.append(produto)
+
+    continuar = input("Deseja cadastrar outro produto? (s/n): ").lower()
+    if continuar == "n":
+        break
+
+# Exibindo os produtos cadastrados
+print("\n--- Produtos cadastrados ---")
+for produto in prateleira:
+    print(f"Produto: {produto['nome']} - Preço: R$ {produto['preco']:.2f} - Setor: {produto['setor']}")
